12b5d27e1aa608fc5e48f730b0fc8a3fc599b4d6
dutao <1173127007@qq.com>
2023-12-18 16:20:56 +0800

    feat: 添加直接复制标的功能

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './demo.less';
 import { AddToPerson, Checked, CheckedChecked, Share } from '../icons';
+import { message, Popover, Button } from 'antd';
 
 const Demo = () => {
+  const copyRef = useRef<HTMLInputElement>(null);
+  const [value, setValue] = useState<string>();
+
+  const onIcon = (text: string) => {
+    setValue(text);
+    setTimeout(() => {
+      copyRef.current?.select();
+      document.execCommand("copy");
+      message.success(`复制${text}成功`);
+    }, 0);
+  }
+
+  const content = (name: string) => {
+    return (
+      <div>
+        <Button onClick={() => onIcon(name)}>复制图标</Button>
+        <Button onClick={() => onIcon(`import { ${name} } from 'react-icons'`)}>复制引入</Button>
+      </div>
+    );
+  };
+
   return (
-    <ul className="demo">
-        <li className="icon">
-          <AddToPerson />
-          <span className="icon-desc">AddToPerson</span>
-        </li>
-        <li className="icon">
-          <Checked />
-          <span className="icon-desc">Checked</span>
-        </li>
-        <li className="icon">
-          <CheckedChecked />
-          <span className="icon-desc">CheckedChecked</span>
-        </li>
-        <li className="icon">
-          <Share />
-          <span className="icon-desc">Share</span>
-        </li>
-    </ul>
+    <>
+      <input 
+        style={{ position: "fixed", left: -100000 }}
+        ref={copyRef}
+        value={value}
+      />
+      <ul className="demo">
+          <Popover content={content("AddToPerson")}>
+            <li className="icon">
+              <AddToPerson />
+              <span className="icon-desc">AddToPerson</span>
+            </li>
+          </Popover>
+          <Popover content={content("Checked")}>
+            <li className="icon">
+              <Checked />
+              <span className="icon-desc">Checked</span>
+            </li>
+          </Popover>
+          <Popover content={content("CheckedChecked")}>
+            <li className="icon">
+              <CheckedChecked />
+              <span className="icon-desc">CheckedChecked</span>
+            </li>
+          </Popover>
+          <Popover content={content("Share")}>
+            <li className="icon">
+              <Share />
+              <span className="icon-desc">Share</span>
+            </li>
+          </Popover>
+      </ul>
+    </>
   )
 }
 
